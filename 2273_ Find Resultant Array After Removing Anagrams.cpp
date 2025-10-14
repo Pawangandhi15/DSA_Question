b2477da3af9755410ba5d54938e93d8e45841d0d
Pawangandhi15 <pawangandhi373@gmail.com>
2025-10-14 09:20:43 +0530

    Question solve

---
+++ 2273_ Find Resultant Array After Removing Anagrams.cpp
@@ -0,0 +1,89 @@
+
+// You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+
+// In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i] are anagrams, and delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies the conditions.
+
+// Return words after performing all operations. It can be shown that selecting the indices for each operation in any arbitrary order will lead to the same result.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all the original letters exactly once. For example, "dacb" is an anagram of "abdc".
+
+ 
+
+// Example 1:
+
+// Input: words = ["abba","baba","bbaa","cd","cd"]
+// Output: ["abba","cd"]
+// Explanation:
+// One of the ways we can obtain the resultant array is by using the following operations:
+// - Since words[2] = "bbaa" and words[1] = "baba" are anagrams, we choose index 2 and delete words[2].
+//   Now words = ["abba","baba","cd","cd"].
+// - Since words[1] = "baba" and words[0] = "abba" are anagrams, we choose index 1 and delete words[1].
+//   Now words = ["abba","cd","cd"].
+// - Since words[2] = "cd" and words[1] = "cd" are anagrams, we choose index 2 and delete words[2].
+//   Now words = ["abba","cd"].
+// We can no longer perform any operations, so ["abba","cd"] is the final answer.
+// Example 2:
+
+// Input: words = ["a","b","c","d","e"]
+// Output: ["a","b","c","d","e"]
+// Explanation:
+// No two adjacent strings in words are anagrams of each other, so no operations are performed.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check_anagram(string &s1, string &s2) {
+        int arr[26] = {0};
+        for (char &ch : s1) {
+            arr[ch - 'a']++;
+        }
+        for (char &ch : s2) {
+            arr[ch - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n = words.size();
+        vector<string> res;
+        res.push_back(words[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (!check_anagram(words[i], res.back())) {
+                res.push_back(words[i]);
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution obj;
+    int n;
+    
+    cout << "Enter number of words: ";
+    cin >> n;
+
+    vector<string> words(n);
+    cout << "Enter the words: ";
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];
+    }
+
+    vector<string> result = obj.removeAnagrams(words);
+
+    cout << "\nWords after removing consecutive anagrams:\n";
+    for (string &w : result) {
+        cout << w << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ 2_ Add Two Numbers.cpp
@@ -0,0 +1,98 @@
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode* ans = new ListNode();
+		ListNode* temp = ans;
+		int carry = 0;
+
+		while (l1 != NULL || l2 != NULL || carry) {
+			int sum = 0;
+			if (l1 != NULL) {
+				sum += l1->val;
+				l1 = l1->next;
+			}
+			if (l2 != NULL) {
+				sum += l2->val;
+				l2 = l2->next;
+			}
+			sum += carry;
+			carry = sum / 10;
+
+			ListNode* temp2 = new ListNode(sum % 10);
+			temp->next = temp2;
+			temp = temp->next;
+		}
+		return ans->next;
+	}
+};
+
+// Helper function to create linked list from vector
+ListNode* createList(vector<int> nums) {
+	ListNode* head = new ListNode(nums[0]);
+	ListNode* curr = head;
+	for (int i = 1; i < nums.size(); i++) {
+		curr->next = new ListNode(nums[i]);
+		curr = curr->next;
+	}
+	return head;
+}
+
+// Helper function to print linked list
+void printList(ListNode* head) {
+	while (head != NULL) {
+		cout << head->val;
+		if (head->next) cout << " -> ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+int main() {
+	Solution obj;
+
+	// Example Input
+	vector<int> num1 = {2, 4, 3};  // represents 342
+	vector<int> num2 = {5, 6, 4};  // represents 465
+
+	ListNode* l1 = createList(num1);
+	ListNode* l2 = createList(num2);
+
+	cout << "First number: ";
+	printList(l1);
+	cout << "Second number: ";
+	printList(l2);
+
+	ListNode* result = obj.addTwoNumbers(l1, l2);
+
+	cout << "\nSum: ";
+	printList(result);
+
+	return 0;
+}
